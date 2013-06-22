ae2f7d2036fb4fd8243f6ff8b4062d87f1ffa4a5
Arash  Sanieyan <ash@peerglobe.com>
2013-06-21 21:22:13 -0700

    added an example package

---
+++ packages/example/site/components/com_helloworld/views/helloworld/html/default.php
@@ -0,0 +1,19 @@
+<?php defined('KOOWA') or die() ?>
+
+<p>
+ Showing a translated text below :
+</p>
+
+<p>
+    <?= @text('COM-HELLOWORLD') ?>
+</p>
+
+<?php if ( $viewer->guest() ) : ?>
+
+I am a guest and don't have a name
+
+<?php else : ?>
+
+My name is <?= @name($viewer) ?>. Nice to meet you.
+
+<?php endif;?>
